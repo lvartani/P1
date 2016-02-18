9cfa218af19d2c26a6d799c212a106da0f3178b7
Loosine Vartani <lvartani.gmail.com>
2016-02-18 02:18:02 -0500

    Added links for P2

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 //global variables
 
- 
+
 // declare namespace
 var dwa = {};
- 
+
 //function that runs when the page loads
-dwa.initialize = function() 
+dwa.initialize = function()
 {
-    
+var p2button = "#p2button";
+$(p2button).click(function(){
+ window.location.href='P2.loosine.com';
+});
 }
- 
